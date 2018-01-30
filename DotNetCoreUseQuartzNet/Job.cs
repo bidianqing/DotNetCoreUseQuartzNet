3b06ba121fa15606d4dcfd1be06bad42f80e5aa6
毕殿卿 <bidq@i-counting.cn>
2018-01-30 12:11:52 +0800

    添加项目文件。

---
+++ DotNetCoreUseQuartzNet/Job.cs
@@ -0,0 +1,14 @@
+﻿using Quartz;
+using System;
+using System.Threading.Tasks;
+
+namespace DotNetCoreUseQuartzNet
+{
+    public class Job : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await Console.Out.WriteLineAsync("Hello World!");
+        }
+    }
+}
---
+++ DotNetCoreUseQuartzNet/Program.cs
@@ -0,0 +1,45 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Threading.Tasks;
+
+namespace DotNetCoreUseQuartzNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunProgram().GetAwaiter().GetResult();
+
+            Console.ReadKey();
+        }
+
+        private static async Task RunProgram()
+        {
+            try
+            {
+                IScheduler scheduler = await StdSchedulerFactory.GetDefaultScheduler();
+
+                await scheduler.Start();
+
+                IJobDetail job = JobBuilder.Create<Job>()
+                    .WithIdentity("job1", "group1")
+                    .Build();
+
+                ITrigger trigger = TriggerBuilder.Create()
+                        .WithIdentity("trigger1", "group1")
+                        .StartNow()
+                        .WithSimpleSchedule(x => x
+                            .WithIntervalInSeconds(10) //每隔10秒执行一次
+                            .RepeatForever())
+                        .Build();
+
+                await scheduler.ScheduleJob(job, trigger);
+            }
+            catch (SchedulerException se)
+            {
+                await Console.Error.WriteLineAsync(se.ToString());
+            }
+        }
+    }
+}
