1d1392e8dbdfd7db6bc9ee635fda34190224a49c
bidianqing <bidianqing@longfor.com>
2023-12-08 18:20:41 +0800

    *

---
+++ DotNetCoreUseQuartzNet/HelloJob.cs
@@ -0,0 +1,14 @@
+﻿using Quartz;
+
+namespace DotNetCoreUseQuartzNet
+{
+    public class HelloJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            context.MergedJobDataMap.TryGetString("phone", out string value);
+
+            await Console.Out.WriteLineAsync($"Hello {value}");
+        }
+    }
+}
--- DotNetCoreUseQuartzNet/Program.cs
+++ DotNetCoreUseQuartzNet/Program.cs
@@ -1,15 +1,13 @@
 ﻿using Quartz;
 using Quartz.Impl;
-using System;
-using System.Threading.Tasks;
 
 namespace DotNetCoreUseQuartzNet
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            RunProgram().GetAwaiter().GetResult();
+            await RunProgram();
 
             Console.ReadKey();
         }
@@ -22,19 +20,29 @@ namespace DotNetCoreUseQuartzNet
 
                 await scheduler.Start();
 
-                IJobDetail job = JobBuilder.Create<Job>()
-                    .WithIdentity("job1", "group1")
+                IJobDetail job = JobBuilder.Create<HelloJob>()
+                    .WithIdentity("hellojob", "default")
                     .Build();
 
                 ITrigger trigger = TriggerBuilder.Create()
-                        .WithIdentity("trigger1", "group1")
                         .StartNow()
+                        .UsingJobData("phone", "120")
                         .WithSimpleSchedule(x => x
                             .WithIntervalInSeconds(10) //每隔10秒执行一次
                             .RepeatForever())
                         .Build();
-
                 await scheduler.ScheduleJob(job, trigger);
+
+                ITrigger trigger2 = TriggerBuilder.Create()
+                        .StartNow()
+                        .ForJob("hellojob", "default")
+                        .UsingJobData("phone", "110")
+                        .WithSimpleSchedule(x => x
+                            .WithIntervalInSeconds(3)
+                            .RepeatForever())
+                        .Build();
+                // 添加一个指定的Job到scheduler，并关联一个触发器
+                await scheduler.ScheduleJob(trigger2);
             }
             catch (SchedulerException se)
             {
